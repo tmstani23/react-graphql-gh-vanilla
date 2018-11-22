cd3f053312b040f7d9cc27c56d95fb7746e9f6a4
tmstani23 <tmstani23@gmail.com>
2018-11-22 10:45:18 -0600

    Update 5
    Refactored main query to include gql variables.  created two new higher order functions to handle making the query request and storing the return data.  Refactored onFetchFromGithub method to use the new higher order functions.  Began work adding comments for each section.

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 const TITLE = 'React GraphQL GitHub Client';
-const getIssuesOfRepositoryQuery = (organization, repository) => `
-  {
-    organization(login: "${organization}") {
+//Graphql query to the github api
+const GET_ISSUES_OF_REPOSITORY = `
+  query ($organization: String!, $repository: String!) {
+    organization(login: $organization) {
       name
       url
-      repository(name: "${repository}") {
+      repository(name: $repository) {
         name
         url
         issues(last: 5) {
@@ -25,16 +26,8 @@ const getIssuesOfRepositoryQuery = (organization, repository) => `
     }
   }
 `;
-
-const axiosGitHubGraphQL = axios.create({
-    baseURL: 'https://api.github.com/graphql',
-    headers: {
-    Authorization: `bearer ${
-    process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
-    }`,
-  },
-});
-
+//Component that displays the errors if any or returns
+  //the organization name and url.  A repository component is also displayed
 const Organization = ({organization, errors}) => {
   if (errors) {
     return (
@@ -55,56 +48,79 @@ const Organization = ({organization, errors}) => {
   </div>
   );
 }
-
+//Displays the repository name and a link to the repository
 const Repository = ({repository}) => (
   <div> 
     <p>
       <strong> In Repository:</strong>  
       <a href={repository.url} > {repository.name} </a>
     </p>
+    {/* each of the first five issues in the repo are mapped as list elements */}
     <ul>
       {repository.issues.edges.map((issue) => (
         <li key={issue.node.id}>  
+          {/* links containing the issue title and url are displayed */}
           <a href={issue.node.url}>{issue.node.title}</a>
         </li>
       ))}
     </ul>
   </div>
 )
+//An axios http request object is created which includes the github api endpoint
+        //and the requester's github authorization token
+const axiosGitHubGraphQL = axios.create({
+  baseURL: 'https://api.github.com/graphql',
+  headers: {
+  Authorization: `bearer ${
+    //The token is saved in an env file to avoid publically displaying the token.
+    process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
+    }`,
+  },
+});
 
+const getIssuesofRepository = path => {
+  //The input path is split at the '/' and each half is saved into separate arrays
+  const [organization, repository] = path.split('/');
+  //The axios post request to github is passed the query and gql variables
+  return axiosGitHubGraphQL.post('', {
+    query: GET_ISSUES_OF_REPOSITORY,
+    variables: { organization, repository },
+    });
+};
+//This function returns an object containing the organization and errors data
+const resolveIssuesQuery = queryResult => () => ({
+  organization: queryResult.data.data.organization,
+  errors: queryResult.data.errors,
+});
+//Main App component which handles rendering, lifecycle and user input.
 class App extends Component {
   state = {
     path: 'the-road-to-learn-react/the-road-to-learn-react',
     organization: null,
     errors: null
   };
-
+  //Call the fetch method using the input field as a path
+    //once the component mounts.
   componentDidMount() {
-    //fetch data
-    
     this.onFetchFromGitHub(this.state.path);
   };
+  //When jsx input field changes update the path state with current value
   onChange = event => {
     this.setState({ path: event.target.value })
   };
+  //When submit button is clicked call on fetchFromgithub component
+    //to start the data request change
   onSubmit = event => {
-    //fetch data
     this.onFetchFromGitHub(this.state.path);
-
+    //prevent default submit button behavior
     event.preventDefault();
   };
   onFetchFromGitHub = path => {
-    //console.log(process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN);
-    //The path is split at the "/" and everything before sign is saved as the organization variable and after saved as the repository variable
-    const [organization, repository] = path.split('/');
-    axiosGitHubGraphQL
-      .post('', { query: getIssuesOfRepositoryQuery(organization, repository)})
-      .then(result => this.setState( () => ({
-        organization: result.data.data.organization,
-        errors: result.data.errors,
-        })),
-      )
-  }
+
+    getIssuesofRepository(path).then(queryResult => 
+      this.setState(resolveIssuesQuery(queryResult)),  
+    );
+  };
   render() {
     const { path, organization, errors } = this.state;
 
